e50e5329673450750e067f696b772395a035bdcd
Mario Martínez Palacio <yonicstudios@gmail.com>
2023-08-22 15:54:59 +0200

    Add base layout

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,23 @@
 import { defineConfig } from 'astro/config';
+import mdx from "@astrojs/mdx";
+import MDXCodeBlocks, { mdxCodeBlockAutoImport } from 'astro-mdx-code-blocks';
+
+import sitemap from "@astrojs/sitemap";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+    site: "https://yonic-corner-legacy.netlify.app",
+    scopedStyleStrategy: "class",
+    experimental: {
+        assets: true
+    },
+    image: {
+        service: { entrypoint: 'astro/assets/services/sharp' },
+    },
+    integrations: [mdx(), sitemap()],
+    vite: {
+        build: {
+            target: "es6"
+        }
+    },
+});
