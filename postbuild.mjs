9c77057ab1ad41a9a0086e5a34e9e5c8368c44cc
Mario Martínez Palacio <yonicstudios@gmail.com>
2024-03-26 21:40:33 +0100

    Add RSS feed

---
+++ postbuild.mjs
@@ -0,0 +1,10 @@
+// Astro does not track additional files generated manually during Astro components rendering,
+// so this script will bundle the static assets that are generated in a hacky way.
+
+import fs from "fs"
+import path from "path"
+
+const src = path.resolve(process.cwd(), "src/feeds/atom.xml");
+const dest = path.resolve(process.cwd(), "dist/feeds/rss.xml");
+
+fs.copyFileSync(src, dest);
