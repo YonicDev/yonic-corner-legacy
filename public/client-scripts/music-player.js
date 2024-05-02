e29f4ec578b944099e7124a335c0a93ccc45fcbe
Mario Martínez Palacio <yonicstudios@gmail.com>
2024-05-02 16:11:49 +0200

    Revert "Add simple version of music player for FP7 for Mac"
    This reverts commit b4e54d6443e34400a27018a7dcc51fd354d19105.

--- public/client-scripts/music-player.js
+++ public/client-scripts/music-player.js
@@ -1,47 +1,50 @@
-function isFullPlayerNotSupported() {
+function isNotSupported() {
     return swfobject.ua.mac && swfobject.getFlashPlayerVersion().major === 7
 }
-
+/*
 function shouldRedirectToStaticObjectPage() {
-    return true //swfobject.ua.mac || (swfobject.ua.win && swfobject.ua.ie);
-}
+    return swfobject.ua.mac || (swfobject.ua.win && swfobject.ua.ie);
+}*/
 
-if (shouldRedirectToStaticObjectPage()) {
+if(isNotSupported()) {
+    document.getElementById("flashContent").innerHTML = "<p>The music player is disabled for Flash Player 7 for Mac due to major issues.</p><p>To activate the music player, use Flash Player 6 instead.</p>"
+} else {// if (shouldRedirectToStaticObjectPage()) {
     var id = window.location.pathname.split("/player-object/")[1];
-    var url = isFullPlayerNotSupported()? "/player-simple/" + id : "/player/" + id;
-    window.location.replace(url);
-} else {
-    // Assuming it's OK to use SWFObject to embed the music player.
+    window.location.href = "/player/" + id;
+} 
 
-    /* Adobe recomienda a los desarrolladores utilizar SWFObject2 para la detección de Flash Player.
-    Para obtener más información, consulte la página del objeto SWFObject en Google Code (http://code.google.com/p/swfobject/).
-    La información también está disponible en el Centro de desarrollo de Adobe, en el artículo sobre "detección de las versiones de Flash Player e incorporación de archivos SWF con SWFObject 2 (en inglés)"
-    Establézcalo en la versión mínima requerida de Flash Player o en 0 si no quiere activar la detección de la versión */
+
+//     // Assuming it's OK to use SWFObject to embed the music player.
+
+//     /* Adobe recomienda a los desarrolladores utilizar SWFObject2 para la detección de Flash Player.
+//     Para obtener más información, consulte la página del objeto SWFObject en Google Code (http://code.google.com/p/swfobject/).
+//     La información también está disponible en el Centro de desarrollo de Adobe, en el artículo sobre "detección de las versiones de Flash Player e incorporación de archivos SWF con SWFObject 2 (en inglés)"
+//     Establézcalo en la versión mínima requerida de Flash Player o en 0 si no quiere activar la detección de la versión */
     
-    var swfVersionStr = "6.0.0";
-    //xiSwfUrlStr se puede utilizar para definir un archivo SWF de instalación rápida.
-    var xiSwfUrlStr = "";
-    var flashvars = {};
-    var flashvars = JSON.parse($("#flashData").html());
-    var params = {};
-    params.quality = "high";
-    params.play = "true";
-    params.loop = "false";
-    params.wmode = "transparent";
-    params.scale = "showall";
-    params.menu = "true";
-    params.devicefont = "false";
-    params.salign = "";
-    params.allowscriptaccess = "sameDomain";
-    var attributes = {};
-    attributes.id = "player";
-    attributes.name = "player";
-    attributes.align = "middle";
-    // swfobject.createCSS("html", "height:100%; background-color: #ffffff;");
-    // swfobject.createCSS("body", "margin:0; padding:0; overflow:hidden; height:100%;");
-    swfobject.embedSWF(
-        "/yonic-corner-music-player.swf", "flashContent",
-        "196", "196",
-        swfVersionStr, xiSwfUrlStr,
-        flashvars, params, attributes);
-}
+//     var swfVersionStr = "6.0.0";
+//     //xiSwfUrlStr se puede utilizar para definir un archivo SWF de instalación rápida.
+//     var xiSwfUrlStr = "";
+//     var flashvars = {};
+//     var flashvars = JSON.parse($("#flashData").html());
+//     var params = {};
+//     params.quality = "high";
+//     params.play = "true";
+//     params.loop = "false";
+//     params.wmode = "transparent";
+//     params.scale = "showall";
+//     params.menu = "true";
+//     params.devicefont = "false";
+//     params.salign = "";
+//     params.allowscriptaccess = "sameDomain";
+//     var attributes = {};
+//     attributes.id = "player";
+//     attributes.name = "player";
+//     attributes.align = "middle";
+//     // swfobject.createCSS("html", "height:100%; background-color: #ffffff;");
+//     // swfobject.createCSS("body", "margin:0; padding:0; overflow:hidden; height:100%;");
+//     swfobject.embedSWF(
+//         "/yonic-corner-music-player.swf", "flashContent",
+//         "196", "196",
+//         swfVersionStr, xiSwfUrlStr,
+//         flashvars, params, attributes);
+// }
