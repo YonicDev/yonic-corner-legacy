2351d882ba0a8dcef7cf530302a33edebf93556a
Mario Martínez Palacio <yonicstudios@gmail.com>
2024-06-09 21:43:37 +0200

    Update to Astro 4.10.1

--- src/pages/feeds/rss.xml.ts
+++ src/pages/feeds/rss.xml.ts
@@ -1,16 +1,12 @@
 import RSSFeed from "@lib/components/feed/RSSFeed.astro";
+import { getContainerRenderer as MDXContainerRenderer } from "@astrojs/mdx"
+import { loadRenderers } from "astro/virtual-modules/container.js"; // Why no "astro:container"?
 import { experimental_AstroContainer as AstroContainer } from "astro/container";
 import type { APIRoute } from "astro";
 
 export const GET: APIRoute = async ({request, locals}) => {
-    const container = await AstroContainer.create({
-        renderers: [
-            {
-                name: "@astrojs/mdx",
-                serverEntrypoint: "astro/jsx/server.js",
-            },
-        ],
-    });
+    const renderers = await loadRenderers([ MDXContainerRenderer()]);
+    const container = await AstroContainer.create({renderers});
 
     const xmlString = await container.renderToString(RSSFeed, { request, locals });
     return new Response(xmlString.replace("<!DOCTYPE html>", ""), {
